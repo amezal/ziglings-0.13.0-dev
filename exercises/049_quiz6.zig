8c7c4064c5b785ea3162d0d96c3b4584499421b8
Armando Meza <amezal01@hotmail.com>
2024-06-14 23:10:45 -0600

    exercise 49

--- exercises/049_quiz6.zig
+++ exercises/049_quiz6.zig
@@ -27,7 +27,13 @@ const Elephant = struct {
     // Your Elephant trunk methods go here!
     // ---------------------------------------------------
 
-    ???
+    pub fn getTrunk(self: *Elephant) *Elephant {
+        return self.trunk.?; // Remember, this means "orelse unreachable"
+    }
+
+    pub fn hasTrunk(self: *Elephant) bool {
+        return (self.trunk != null);
+    }
 
     // ---------------------------------------------------
 
